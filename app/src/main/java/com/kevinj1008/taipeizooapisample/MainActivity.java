0892e216c4fa461e6687b4023adbf564f9651774
KevinJ1008 <car4353666@gmail.com>
2019-03-19 16:21:16 +0800

    Build empty project and build base activity

---
+++ app/src/main/java/com/kevinj1008/taipeizooapisample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kevinj1008.taipeizooapisample;
+
+import android.os.Bundle;
+
+import com.kevinj1008.taipeizooapisample.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/kevinj1008/taipeizooapisample/base/BaseActivity.java
@@ -0,0 +1,35 @@
+package com.kevinj1008.taipeizooapisample.base;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected Context mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.mContext = this;
+
+        setStatusBar();
+    }
+
+    private void setStatusBar() {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);//calculateStatusColor(Color.WHITE, (int) alphaValue)
+    }
+}
