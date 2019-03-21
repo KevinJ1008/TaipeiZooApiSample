a171ac20f908cb1e39f0900950f7f5eabc2c7332
KevinJ1008 <car4353666@gmail.com>
2019-03-21 17:39:57 +0800

    Make api helper

--- app/src/main/java/com/kevinj1008/taipeizooapisample/api/ApiHelper.java
+++ app/src/main/java/com/kevinj1008/taipeizooapisample/api/ApiHelper.java
@@ -1,36 +1,51 @@
 //package com.kevinj1008.taipeizooapisample.api;
 //
+//import android.util.Log;
+//
 //import com.kevinj1008.taipeizooapisample.api.bean.GetZoos;
 //import com.kevinj1008.taipeizooapisample.model.Zoo;
+//import com.kevinj1008.taipeizooapisample.model.response.ZooResponse;
+//import com.kevinj1008.taipeizooapisample.model.result.ZooResult;
+//import com.kevinj1008.taipeizooapisample.util.Constants;
 //
 //import java.io.IOException;
-//import java.util.ArrayList;
 //
 //import retrofit2.Call;
 //import retrofit2.Callback;
-//import retrofit2.ZooResponse;
+//import retrofit2.Response;
 //import retrofit2.Retrofit;
+//import retrofit2.converter.gson.GsonConverterFactory;
 //
 //public class ApiHelper {
 //
 //
-//    public static GetZoos getZoos() throws IOException {
+//    public static GetZoos getZoos() {
+//        final GetZoos zoos = new GetZoos();
 //
-//        Retrofit retrofit = new Retrofit.Builder().build();
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl(ApiConstants.BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
 //        ApiService service = retrofit.create(ApiService.class);
-//        Call<ArrayList<Zoo>> call = service.getZoos();
-//        call.enqueue(new Callback<ArrayList<Zoo>>() {
+//        Call<ZooResponse> call = service.getZooResponse();
+//        call.enqueue(new Callback<ZooResponse>() {
 //            @Override
-//            public void onResponse(Call<ArrayList<Zoo>> call, ZooResponse<ArrayList<Zoo>> response) {
-//                ArrayList<Zoo> zoos = response.body();
+//            public void onResponse(Call<ZooResponse> call, Response<ZooResponse> response) {
+//                ZooResponse zooResponse = response.body();
+//                Log.d(Constants.TAG, "Get ZooResponse: " + zooResponse.toString());
 //
+//                ZooResult zooResult = zooResponse.getZooResult();
+//                Log.d(Constants.TAG, "Get ZooResult: " + zooResult.toString());
+//                zoos.getZoos().addAll(zooResult.getZoos());
 //            }
 //
 //            @Override
-//            public void onFailure(Call<ArrayList<Zoo>> call, Throwable t) {
-//
+//            public void onFailure(Call<ZooResponse> call, Throwable t) {
+//                Log.d(Constants.TAG, "Api fail: " + t.getMessage());
 //            }
 //        });
 //
+//        return zoos;
 //    }
 //}
